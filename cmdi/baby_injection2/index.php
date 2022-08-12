1c79aa38e6e0ce3ed02fa4b79342e87fd7253426
eethan1 <call132call132@gmail.com>
2022-08-12 10:30:28 +0800

    fix baby

--- cmdi/baby_injection2/index.php
+++ cmdi/baby_injection2/index.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_GET['chapter'])) {
-    if (preg_match('/[\(\) ;\n\t\r`\.#\\\'"?]|flag/', $_GET['chapter'])) {
+    if (preg_match('/[\(\) ;\n\t\r`\.&|<>#\\\'"?]|flag/', $_GET['chapter'])) {
         die('bad hacker');
     }
     system("bash -c 'cat chapter{" . $_GET['chapter'] . "}.txt'");
